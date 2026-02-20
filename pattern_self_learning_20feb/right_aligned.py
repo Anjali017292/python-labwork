bf1a5b830f4cce6d6c11bc04a9298a777db831f4
Anjali017292 <jattianjali1703@gmail.com>
2026-02-20 22:07:00 +0530

    this file contain right aligned  program with output

---
+++ pattern_self_learning_20feb/right_aligned.py
@@ -0,0 +1,16 @@
+# Right Aligned Triangle Pattern
+rows = 5
+for i in range(1, rows+1):
+    for j in range(rows-i):
+        print(" ", end=" ")
+    for k in range(i):
+        print("*", end=" ")
+    print()
+    
+'''output:
+        *
+        * *
+        * * *
+        * * * *
+        * * * * *
+'''
